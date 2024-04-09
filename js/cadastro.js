8202cf9ce4b38827de8df18ca4324eae393e26cc
Andre Nascimento <1914andre@gmail.com>
2024-04-09 14:09:16 -0300

    feat:criando usuarios

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,5 +1,34 @@
-const clicar = document.getElementById("btn")
-clicar.addEventListener("click",()=>{
-    alert("clicou")
-})
+const usuarios=[
+    {
+        login: '1914Andre@gmail.com',
+        pass: 'A19551947b'
+    },
+
+    {
+        login: 'MonicaInchada',
+        pass: '123456Senhaforte'
+    },
+
+
+    {
+        login: 'EleonorProgramador',
+        pass: 'MaquinaDeLavarBarata123'
+    },
+
+    {
+        login: 'AldeaoRonaldo',
+        pass: 'Mojang2012'
+    },
+
+    {
+        login: 'JustinBebado',
+        pass: 'S0rry'
+    },
+
+    {
+        login: '',
+        pass: ''
+    },
+]
+
 
