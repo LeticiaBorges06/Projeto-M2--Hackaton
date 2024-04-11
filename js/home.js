5a2a97587b22053ac5332b657916497d3e4747ff
julianascimentovieira <157746817+julianascimentovieira@users.noreply.github.com>
2024-04-10 22:57:47 -0300

    adicionando html,css e js do home

---
+++ js/home.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Seleciona o botão de Cadastro
+    const registerBtn = document.getElementById('register');
+    // Adiciona um evento de clique ao botão de Cadastro
+    registerBtn.addEventListener('click', function (event) {
+        // Previne o comportamento padrão do botão (neste caso, para seguir o link)
+        event.preventDefault();
+        // ele simula o redirecionamento para a página de cadastro
+        alert('Redirecionando para a página de cadastro...');
+    });
+
+    // Seleciona o botão de Entrar como Visitante
+    const visitorBtn = document.getElementById('visitor');
+    // Adiciona um evento de clique ao botão de Entrar como Visitante
+    visitorBtn.addEventListener('click', function (event) {
+        // Previne o comportamento padrão do botão (neste caso,para seguir o link)
+        event.preventDefault();
+        // ele Simula o login como visitante
+        alert('Entrando como visitante...');
+    });
+});
